ebfa7805e4cf63cd7a9c61f950ba6dcbf00d3dcd
subuta <shunta.3110@gmail.com>
2017-12-04 17:50:02 +0900

    Fix cached dependency.

--- generators/example.js
+++ generators/example.js
@@ -1,4 +1,6 @@
+import helper from './_helper'
+
 export default (ctx) => {
   const { filePath, fileName, fs } = ctx
-  return fs.writeFile(`${filePath}/${fileName}`, `console.log('generated file');`)
+  return fs.writeFile(`${filePath}/${fileName}`, `console.log('generated file, helper.default = ${helper}');`)
 }

adb569305c72861d6dd8f23f35c264b0a60d160b
subuta <shunta.3110@gmail.com>
2018-01-13 18:47:32 +0900

    Add example generator.

---
+++ front/generators/hoge.js
@@ -0,0 +1,10 @@
+import { build, snippets as s } from 'bld.js'
+import Action from '@subuta/snippets/redux/Action'
+
+export default (ctx) => {
+  const {filePath, fileName, fs} = ctx
+  return fs.writeFile(`${filePath}/${fileName}`, build`
+    ${Action()}
+    testsaass
+  `)
+}
